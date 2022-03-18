226fb0e22403aa64b4ec9c6b83ec0ada03ba709f
aearns <aearns@github.com>
2022-03-17 20:48:57 -0700

    edited

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,14 @@
  *
  * Return: 0-9 followed by newline
  */
-void print_numbers(void)
+int main(void) 
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
 		_putchar(i + '0');
 	_putchar('\n');
+
+	return (0);
 }
+
