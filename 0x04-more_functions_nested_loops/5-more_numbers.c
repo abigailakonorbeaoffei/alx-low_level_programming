f2316d0a5d924f4b99d3190ffac683d80f11abf1
aearns <aearns@github.com>
2022-03-19 06:14:01 -0700

    Changes

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+/*
+ * Print 10 times numbers from 
+ * 0 - 14 on a new line
+ * */
+void more_numbers(void)
+{
+	int i = 0; int i >= 14;
+	{
+		_putchar(i; '\n');
+		
+		i++;
+	}
+
+	return (0);
+}
